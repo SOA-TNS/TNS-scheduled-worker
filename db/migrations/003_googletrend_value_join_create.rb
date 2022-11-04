4f7c3284bd194734162b4a66c79cd2d95fce74de
Yu-Hsuan <44157186+ZweiChen0328@users.noreply.github.com>
2022-11-05 00:58:56 +0800

    Update 003_googletrend_value_join_create.rb

--- db/migrations/003_googletrend_value_join_create.rb
+++ db/migrations/003_googletrend_value_join_create.rb
@@ -5,9 +5,9 @@ require 'sequel'
 Sequel.migration do
   change do
     create_table(:googletrend_values) do
-      primary_key [:stock_id, :query] # rubocop:disable Style/SymbolArray
+      primary_key [:stock_id, :value_id] # rubocop:disable Style/SymbolArray
       foreign_key :stock_id, :stock
-      foreign_key :query, :gtvalues
+      foreign_key :value_id, :gtvalues
 
       index [:stock_id, :query] # rubocop:disable Style/SymbolArray
     end
