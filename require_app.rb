1aed904a63161bee739715c40d39222781b44b3e
Jerryeeeh <107199830+Jerryeeeh@users.noreply.github.com>
2022-11-05 20:04:16 +0800

    Add files via upload

--- require_app.rb
+++ require_app.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 # Requires all ruby files in specified app folders
-def require_app(folders = %w[infrastructure models views controllers])
+def require_app(folders= %w[infrastructure models views controllers])
   app_list = Array(folders).map { |folder| "app/#{folder}" }
   full_list = ['config', app_list].flatten.join(',')
+  puts full_list
 
   Dir.glob("./{#{full_list}}/**/*.rb").each do |file|
     require file
