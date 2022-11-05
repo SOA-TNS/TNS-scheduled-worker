724e18cc991730ef4aa19a9045cea6a6d2d7a2c2
ricky871029 <62226849+ricky871029zzz@users.noreply.github.com>
2022-11-05 22:21:11 +0800

    Add files via upload

--- app/models/repositories/rgt_entities.rb
+++ app/models/repositories/rgt_entities.rb
@@ -1,31 +1,15 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
+
+require 'dry-types'
+require 'dry-struct'
 
 module GoogleTrend
-  module Repository
-    # Repository for Members
-    class RgtEntities
-      def self.find_id(id)
-        rebuild_entity Database::StockOrm.first(id:)
-      end
-
-      def self.find_username(stock_name)
-        rebuild_entity Database::StockOrm.first(stock_name:)
-      end
-
-      def self.rebuild_entity(db_record)
-        return nil unless db_record
-
-        Entity::RgtEntity.new(
-          query: ,
-          time_series: 
-        )
-      end
-
-      def self.rebuild_many(db_records)
-        db_records.map do |db_member|
-          RgtEntities.rebuild_entity(db_member)
-        end
-      end
+  module Entity
+    class RgtEntity < Dry::Struct
+      include Dry.Types
+  
+      attribute :query, Strict::String
+      attribute :time_series, Strict::Hash   #Array
     end
   end
 end
