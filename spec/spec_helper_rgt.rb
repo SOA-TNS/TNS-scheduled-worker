78eefd36c9d49d30bb62a02af6ef0c9f6c557550
Jerryeeeh <107199830+Jerryeeeh@users.noreply.github.com>
2022-11-06 19:44:22 +0800

    Add files via upload

--- spec/spec_helper_rgt.rb
+++ spec/spec_helper_rgt.rb
@@ -12,8 +12,15 @@ require 'webmock'
 require_relative '../require_app'
 require_app
 
+def time_value
+    l = []
+    a = @rgt["interest_over_time"] #hash
+    b = a["timeline_data"] #array
+    b.each{ |data| l << "#{data["date"]} => #{data["values"][0]["value"]}"  }
+    l.to_s
+end
+
 RGT_TOKEN = GoogleTrend::App.config.RGT_TOKEN
 CORRECT = YAML.safe_load(File.read('spec/fixtures/rgt_results.yml'))
-
 CASSETTES_FOLDER = 'spec/fixtures/cassettes'
 CASSETTE_FILE = 'rgt_api'
