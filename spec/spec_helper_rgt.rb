a25c522aefc9137788ad53eb2adc4aebb40f1fcd
Jerryeeeh <107199830+Jerryeeeh@users.noreply.github.com>
2022-11-20 18:34:05 +0800

    update spec

--- spec/spec_helper_rgt.rb
+++ spec/spec_helper_rgt.rb
@@ -13,11 +13,11 @@ require_relative '../require_app'
 require_app
 
 def time_value(file)
-  l = []
-  a = file['interest_over_time'] # hash
-  b = a['timeline_data'] # array
-  b.each { |data| l << "#{data['date']} => #{data['values'][0]['value']}" }
-  l.to_s
+  array = []
+  time_series = file['interest_over_time'] # hash
+  interest_over_time = interest_over_time['timeline_data'] # array
+  interest_over_time.each { |data| array << "#{data['date']} => #{data['values'][0]['value']}" }
+  array.to_s
 end
 
 RGT_TOKEN = GoogleTrend::App.config.RGT_TOKEN
