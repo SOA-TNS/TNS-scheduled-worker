0a1d82e2a58bea8b1296c5b869979b13f58aec90
Jerryeeeh <107199830+Jerryeeeh@users.noreply.github.com>
2022-11-22 22:47:28 +0800

    modify mapper

--- app/domain/models/mappers/Stock_InstitutionalInvestorsBuySell_Mapper.rb
+++ app/domain/models/mappers/Stock_InstitutionalInvestorsBuySell_Mapper.rb
@@ -1,67 +1,66 @@
 # frozen_string_literal: true
 
 module GoogleTrend
-    module Gt
-      class StockInstitutionalInvestorsBuySellMapper
-  
-        def initialize( data_id, start_date = (Time.now - 3600*24*60).to_s[0..9], end_date = Time.now.to_s[0..9], gateway_class = Gt::StockApi)
-          @data_id = data_id
-          @gateway_class = gateway_class
-          @start_date = start_date
-          @end_date = end_date
-          @gateway = @gateway_class.new("TaiwanStockInstitutionalInvestorsBuySell", @data_id, @start_date, @end_date)
-        end
-  
-        def find
-          stock = @gateway.jason
-          build_entity(stock)
-        end
-  
-        def build_entity(stock)
-          DataMapper.new(stock).build_entity
-        end
-  
-        class DataMapper
-  
-          def initialize(stock)
-            @stock = stock
-          end
-  
-          def build_entity
-            GoogleTrend::Entity::StockInstitutionalInvestorsBuySellEntity.new(
-              id: nil,
-              stock_name:,
-              name:,
-              per:,
-              pbr:
-            )
-          end
-  
-          def stock_name
-            @stock["data"][0]["stock_id"]
-          end
-  
-          def name
-            name = []
-            value_over_time = @stock["data"] #hash
-            value_over_time.each{ |value| name << value["name"] } #array
-            name.to_s
-          end
+  module Gt
+    class StockInstitutionalInvestorsBuySellMapper
+      def initialize(data_id, start_date = (Time.now - 3600 * 24 * 60).to_s[0..9], end_date = Time.now.to_s[0..9],
+                     gateway_class = Gt::StockApi)
+        @data_id = data_id
+        @gateway_class = gateway_class
+        @start_date = start_date
+        @end_date = end_date
+        @gateway = @gateway_class.new('TaiwanStockInstitutionalInvestorsBuySell', @data_id, @start_date, @end_date)
+      end
 
-          def buy
-            buy = []
-            value_over_time = @stock["data"] #hash
-            value_over_time.each{ |value| buy << value["buy"] } #array
-            buy.to_s
-          end
-          
-          def sell
-            sell = []
-            value_over_time = @stock["data"] #hash
-            value_over_time.each{ |value| sell << value["sell"] } #array
-            sell.to_s
-          end  
+      def find
+        stock = @gateway.jason
+        build_entity(stock)
+      end
+
+      def build_entity(stock)
+        DataMapper.new(stock).build_entity
+      end
+
+      class DataMapper
+        def initialize(stock)
+          @stock = stock
+        end
+
+        def build_entity
+          GoogleTrend::Entity::StockInstitutionalInvestorsBuySellEntity.new(
+            id: nil,
+            stock_name:,
+            name:,
+            per:,
+            pbr:
+          )
+        end
+
+        def stock_name
+          @stock['data'][0]['stock_id']
+        end
+
+        def name
+          name = []
+          value_over_time = @stock['data'] # hash
+          value_over_time.each { |value| name << value['name'] } # array
+          name.to_s
+        end
+
+        def buy
+          buy = []
+          value_over_time = @stock['data'] # hash
+          value_over_time.each { |value| buy << value['buy'] } # array
+          buy.to_s
+        end
+
+        def sell
+          sell = []
+          value_over_time = @stock['data'] # hash
+          value_over_time.each { |value| sell << value['sell'] } # array
+          sell.to_s
         end
       end
     end
   end
+end
--- app/domain/models/mappers/stock_per_mapper.rb
+++ app/domain/models/mappers/stock_per_mapper.rb
@@ -1,71 +1,71 @@
 # frozen_string_literal: true
+
 require_relative 'findmind_api'
 
 module GoogleTrend
-    module Gt
-      class StockPerMapper
-  
-        def initialize( data_id, start_date = (Time.now - 3600*24*60).to_s[0..9], end_date = Time.now.to_s[0..9], gateway_class = Gt::StockApi)
-          @data_id = data_id
-          @gateway_class = gateway_class
-          @start_date =start_date
-          @end_date = end_date
-          @gateway = @gateway_class.new("TaiwanStockPER", @data_id, @start_date, @end_date)
-        end
-  
-        def find
-          stock = @gateway.jason
-          build_entity(stock)
-        end
-  
-        def build_entity(stock)
-          DataMapper.new(stock).build_entity
-        end
-  
-        class DataMapper
-  
-          def initialize(stock)
-            @stock = stock
-          end
-  
-          def build_entity
-            GoogleTrend::Entity::StockPerEntity.new(
-              id: nil,
-              stock_name:,
-              time:,
-              per:,
-              pbr:
-            )
-          end
-  
-          def stock_name
-            @stock["data"][0]["stock_id"]
-          end
-  
-          def time
-            date = []
-            value_over_time = @stock["data"] #hash
-            value_over_time.each{ |value| date << value["date"] } #array
-            date.to_s
-          end
+  module Gt
+    class StockPerMapper
+      def initialize(data_id, start_date = (Time.now - 3600 * 24 * 60).to_s[0..9], end_date = Time.now.to_s[0..9],
+                     gateway_class = Gt::StockApi)
+        @data_id = data_id
+        @gateway_class = gateway_class
+        @start_date = start_date
+        @end_date = end_date
+        @gateway = @gateway_class.new('TaiwanStockPER', @data_id, @start_date, @end_date)
+      end
 
-          def per
-            per = []
-            value_over_time = @stock["data"] #hash
-            value_over_time.each{ |value| per << value["PER"] } #array
-            per.to_s
-          end
-          
-          def pbr
-            pbr = []
-            value_over_time = @stock["data"] #hash
-            value_over_time.each{ |value| pbr << value["PBR"] } #array
-            pbr.to_s
-          end  
+      def find
+        stock = @gateway.jason
+        build_entity(stock)
+      end
+
+      def build_entity(stock)
+        DataMapper.new(stock).build_entity
+      end
+
+      class DataMapper
+        def initialize(stock)
+          @stock = stock
+        end
+
+        def build_entity
+          GoogleTrend::Entity::StockPerEntity.new(
+            id: nil,
+            stock_name:,
+            time:,
+            per:,
+            pbr:
+          )
+        end
+
+        def stock_name
+          @stock['data'][0]['stock_id']
+        end
+
+        def time
+          date = []
+          value_over_time = @stock['data'] # hash
+          value_over_time.each { |value| date << value['date'] } # array
+          date.to_s
+        end
+
+        def per
+          per = []
+          value_over_time = @stock['data'] # hash
+          value_over_time.each { |value| per << value['PER'] } # array
+          per.to_s
+        end
+
+        def pbr
+          pbr = []
+          value_over_time = @stock['data'] # hash
+          value_over_time.each { |value| pbr << value['PBR'] } # array
+          pbr.to_s
         end
       end
     end
   end
+end
 
-  p = GoogleTrend::Gt::StockPerMapper.new("2330").find
-  print(p)
+p = GoogleTrend::Gt::StockPerMapper.new('2330').find
+print(p)
