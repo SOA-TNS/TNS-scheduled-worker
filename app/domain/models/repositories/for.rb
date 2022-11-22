b9da349f9d3f5a7e5f562a93b1adee736a5e43c3
ricky871029 <62226849+ricky871029zzz@users.noreply.github.com>
2022-11-22 09:24:32 +0800

    find the right repository for a given entity

---
+++ app/domain/models/repositories/for.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative 'stock'
+
+module GoogleTrend
+  module Repository
+    # Finds the right repository for an entity object or class
+    module For
+      ENTITY_REPOSITORY = {
+        Entity::RgtEntity => Stock
+      }.freeze
+
+      def self.klass(entity_klass)
+        ENTITY_REPOSITORY[entity_klass]
+      end
+
+      def self.entity(entity_object)
+        ENTITY_REPOSITORY[entity_object.class]
+      end
+    end
+  end
+end
+
