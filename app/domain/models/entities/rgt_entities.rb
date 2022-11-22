df41d3a74815816faafb1da61fc785ffe80c0576
ricky871029 <62226849+ricky871029zzz@users.noreply.github.com>
2022-11-22 09:20:19 +0800

    Google Trend Entities

---
+++ app/domain/models/entities/rgt_entities.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: false
+
+require 'dry-types'
+require 'dry-struct'
+
+module GoogleTrend
+  module Entity
+    class RgtEntity < Dry::Struct
+      include Dry.Types
+  
+      attribute :id,        Integer.optional
+      attribute :query,     Strict::String
+      attribute :time_series, Strict::String   #Array
+      
+      def to_attr_hash
+        to_hash.except(:id)
+      end
+    end
+  end
+end
