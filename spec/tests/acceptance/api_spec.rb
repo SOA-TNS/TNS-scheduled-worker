94e16feff0ef77cf8b6e866713fa1b0f31d541d3
ricky871029 <62226849+ricky871029zzz@users.noreply.github.com>
2022-12-04 23:14:58 +0800

    api_spec

--- spec/tests/acceptance/api_spec.rb
+++ spec/tests/acceptance/api_spec.rb
@@ -55,3 +55,57 @@ describe 'Test API routes' do
       _(JSON.parse(last_response.body)['status']).must_include 'not'
     end
   end
+
+  describe 'Add stock route' do
+    it 'should be able to add a stock' do
+      post "api/v1/Gtrend/#{QUERY}"
+      _(last_response.status).must_equal 201
+
+      stock = JSON.parse last_response.body
+      _(stock['query']).must_equal QUERY
+      _(stock['time_series']).must_equal TIME_SERIES
+    end
+
+    it 'should report error for invalid stock' do
+      post 'api/v1/Gtrend/vnhsiufbnisubnsre'
+      _(last_response.status).must_equal 404
+
+      response = JSON.parse(last_response.body)
+      _(response['message']).must_include 'not'
+    end
+  end
+
+  describe 'Get stocks list' do
+    it 'should successfully return stock lists' do
+      
+      get "api/v1/Gtrend?list=ewogICJsaXN0IjpbImFwcGxlIiwiYmFuYW5hIl0KfQ=="
+      _(last_response.status).must_equal 200
+
+      response = JSON.parse(last_response.body)
+      
+      stocks = response['stocks']
+      _(stocks.count).must_equal 2
+      stock = stocks.first
+      _(stock['query']).must_equal QUERY
+    end
+
+    it 'should return empty lists if none found' do
+
+      get "/api/v1/Gtrend?list=ewogICJsaXN0IjpbImNhdCJdCn0="
+      _(last_response.status).must_equal 200
+
+      response = JSON.parse(last_response.body)
+      stocks = response['stocks']
+      _(stocks).must_be_kind_of Array
+      _(stocks.count).must_equal 0
+    end
+
+    it 'should return error if not list provided' do
+      get '/api/v1/Gtrend'
+      _(last_response.status).must_equal 400
+
+      response = JSON.parse(last_response.body)
+      _(response['message']).must_include 'list'
+    end
+  end
+end
