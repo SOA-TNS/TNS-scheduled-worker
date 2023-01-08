674fe68c75ece752903701d268d627f07645c7e7
Yu-Hsuan <44157186+ZweiChen0328@users.noreply.github.com>
2023-01-08 22:48:05 +0800

    Delete app directory

--- app/presentation/responses/stock_mainpage.rb
+++
@@ -1,9 +0,0 @@
-
-# frozen_string_literal: true
-
-module GoogleTrend
-  module Response
-    StockInfo = Struct.new(:data_record, :risk)
-  end
-end
-# in folder "responses"
--- app/presentation/responses/stocks_rgt.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module GoogleTrend
-    module Response
-      StocksList = Struct.new(:stocks)
-    end
-  end
